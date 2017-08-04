742659d18f185242b2c3e8f7d23534267dd0a561
James Wolff <JWolff@SemGeeks.local>
2017-08-04 11:07:36 -0400

    Made changes to footer, header, measurements, and styling

--- header.php
+++ header.php
@@ -9,6 +9,21 @@
  * @package wolffpress
  */
 
+/*
+
+IMPORTANT CLASSES:
+
+Content in the following classes will be shifted left right or center based on the variables in _measurements.scss:
+header-left 
+header-center
+header-right
+
+Use this class in conjunction with header-left, header-center, or header-right to denote if a section is used for the logo.
+This will prevent the logo from shrinking beyond the min-width set in _measurements.scss:
+header-logo
+
+*/
+
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -24,10 +39,12 @@
 	<header id="masthead" class="site-header" role="banner">
 
 		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'wolffpress' ); ?></a>
+		<!-- Use header-left, header-center, header-right inside header-wrapper to organize your header based on the variables located in _measurements.scss  -->
 		<div class="header-wrapper">
-			<div class="header-left"><a class="brand" href="/"><img src="<?= get_template_directory_uri() . '/assets/images/logo.png' ?>"></a></div>
-
-				<nav id="site-navigation" class="header-center" role="navigation">
+			<!-- The header-logo class is a class that goes on the header section where the logo is housed. This applies the logo's min-width to that container -->
+			<div class="header-left header-logo"><a href="/"><img src="<?= get_template_directory_uri() . '/assets/images/logo.png' ?>"></a></div>
+			<div class="header-center">
+				<nav id="site-navigation" role="navigation">
 					<div id="hamburger-menu" class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
 						<div class="hamburger-top"></div>
 						<div class="hamburger-middle"></div>
@@ -35,7 +52,7 @@
 					</div>
 					<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
-
+			</div>
 			<div class="header-right">
 				<div class="phone"><a href="tel:5555555555">(555) 555 5555</a></div>
 				<div class="email"><a href="mailto:admin@email.com">admin@email.com</a></div>
